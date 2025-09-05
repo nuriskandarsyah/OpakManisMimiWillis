09cbc1d47807b05681bfe202d9f3b50eaadcb1c8
Nur Iskandar Syah <nuriskandarsyah010@gmail.com>
2025-09-05 10:24:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CVController;
 
-Route::get('/', [CVController::class, 'karikop'])->name('karikop');
-Route::get('/ProdukUmi', [CVController::class, 'ProdukUmi'])->name('produkumi');
-Route::get('/IstyPJ', [CVController::class, 'IstyPJ'])->name('istypj');
+Route::get('/', [CVController::class, 'index'])->name('index');
+Route::get('/opak', [CVController::class, 'opak'])->name('opak');
+
